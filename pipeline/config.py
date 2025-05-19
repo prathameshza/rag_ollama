ce63788f7e2707fdc0b8fbc614db2e2273deb507
Prathamesh Zade <prathmeshpaware9090@gmail.com>
2025-05-19 17:30:29 +0530

    Create config.py

---
+++ pipeline/config.py
@@ -0,0 +1,10 @@
+MYSQL_CONFIG = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "12345",
+    "db": "demo_db"
+}
+
+OLLAMA_API_BASE = "http://localhost:11434/v1"
+OLLAMA_MODEL = "mistral:instruct"
